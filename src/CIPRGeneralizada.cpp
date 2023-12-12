9813922265a926ac46dbb6128f196b2b58ff7be5
AtilaJunior <106200613+AtilaJunior@users.noreply.github.com>
2023-12-12 06:52:22 -0300

    Add files via upload

--- src/CIPRGeneralizada.cpp
+++ src/CIPRGeneralizada.cpp
@@ -10,7 +10,7 @@ void CIPRGeneralizada::CalcIPR(CFluido* fluido, CReservatorio& reservatorio, CPo
     // Inicializa a classe CIPRVariacaoPwf
     iprVariacaoPwf(pi);
 
-    if (pi <= pb) {
+    if (pi >= pb) {
         // Loop para calcular a vazao para cada valor de Pwf
         for (double pwf : valoresPwf) {
             vazao.push_back(j * (pi - pb) + ((j * pb) / 1.8) * (1.0 - 0.2 * (pwf / pb) - 0.8 * ((pwf / pb) * (pwf / pb))));
@@ -18,7 +18,6 @@ void CIPRGeneralizada::CalcIPR(CFluido* fluido, CReservatorio& reservatorio, CPo
             std::cout << "Pwf: " << pwf << ", Vazao: " << j * (pi - pwf) << std::endl;
         }
     } else {
-        // Mostrar mensagem de erro e encerrar o codigo
         for (double pwf : valoresPwf) {
             vazao.push_back(j * (pi - pwf));
 
